13702f43934222029522b0ec052dac54983d1bd8
MCEScript <94685251+MCEScript@users.noreply.github.com>
2021-12-06 17:25:17 +0700

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,7 +1,7 @@
 local key = _G.Key
-local check = "checkhere?key=" .. key
+local check = "https://raw.githubusercontent.com/MCEScript/Script_MCE_HUB/main/Check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
 loadstring(game:HttpGet("scripthere"))()
 else
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+game.Players.LocalPlayer:Kick("Key ผิดกรุณาตรวจสอบให้ดีก่อนใส่ <3 .")
 end
