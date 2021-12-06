e7795a6e7fd4cf728030d3a228cbd030fbed5cba
MCEScript <94685251+MCEScript@users.noreply.github.com>
2021-12-06 18:16:52 +0700

    Update Check.php

--- Check.php
+++ Check.php
@@ -15,7 +15,7 @@ $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 $fh = fopen('logs.txt', 'a'); 
 fwrite($fh, ''."".$ip ."\n");
 $keys = array(
-"CHIZURU",
+"JX9S-ZJXA-JCXN-JZ2",
 
 "MASHIRO",
 ); 
