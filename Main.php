ca4341ad87eed743001c3be603312f106e8768d0
MCEScript <94685251+MCEScript@users.noreply.github.com>
2021-12-06 17:29:19 +0700

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,7 +1,7 @@
 local key = _G.Key
 local check = "https://raw.githubusercontent.com/MCEScript/Script_MCE_HUB/main/Check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://github.com/MCEScript/Script_MCE_HUB/blob/main/Main-Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MCEScript/Script_MCE_HUB/main/Main-Script.lua"))()
 else
 game.Players.LocalPlayer:Kick("Key ผิดกรุณาตรวจสอบให้ดีก่อนใส่ <3 .")
 end
